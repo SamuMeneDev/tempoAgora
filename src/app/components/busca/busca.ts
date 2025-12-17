d252f9d94833d61b21937de0a946c3e2a97f8a19
Samu Mene <samuelsantiago2222@gmail.com>
2025-12-17 01:47:21 -0300

    requisição das localidades

--- src/app/components/busca/busca.ts
+++ src/app/components/busca/busca.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-busca',
   imports: [],
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './busca.css',
 })
 export class Busca {
+  
+  constructor() {
+    const dados = JSON.parse(this.carregarEstados());
+  }
 
+  public carregarEstados() {
+    const request = new XMLHttpRequest();
+    request.open("GET", 'https://servicodados.ibge.gov.br/api/v1/localidades/estados', false);
+    request.send();
+    return request.responseText;
+  }
 }

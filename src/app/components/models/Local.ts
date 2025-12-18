2bb856d07627c8d01c57c79dba52b39c20b63ad9
Samu Mene <samuelsantiago2222@gmail.com>
2025-12-18 12:42:42 -0300

    Funções de conversão de horário e ajustes de UI

--- src/app/components/models/Local.ts
+++ src/app/components/models/Local.ts
@@ -1,12 +1,43 @@
 import Cidade from './Cidade';
 import Estado from './Estado';
 import { environment } from '../../../environments/environment';
+interface IUvDados {
+  cor: string;
+  nivel: string;
+  dica: string;
+}
 export default class Local {
   private estado!: Estado;
   private cidade!: Cidade;
   private status: boolean = false;
   private clima!: Clima;
-
+  private uvDados: IUvDados[] = [
+    {
+      cor: '',
+      nivel: 'Baixo',
+      dica: 'Nenhuma precaução necessária. Procure uma combra nas horas próximas ao meio-dia.',
+    },
+    {
+      cor: '',
+      nivel: 'Moderado',
+      dica: 'Em horários próximos ao meio-dia, procure locais sombreados. Procure usar camisa e boné. User o protetor solar.',
+    },
+    {
+      cor: '',
+      nivel: 'Alto',
+      dica: 'Risco Alto. User protetor solar FPS alto, em horários próximos ao meio-dia, procure locais sombreados. Procure usar camisa, boné e óculos de sol.',
+    },
+    {
+      cor: '',
+      nivel: 'Muito Alto',
+      dica: 'Risco muito alto. Evite o sol em horários próximos ao meio-dia, use protetor FPS 50+, chapéu, óculos de sol e roupa'
+    },
+    {
+      cor: '',
+      nivel: 'Extremo',
+      dica: 'Risco extremo. Evite exposição ao sol a todo custo. Mesmo curtos períodos causam queimaduras graves'
+    }
+  ];
   constructor() {
     this.reset();
   }
@@ -65,5 +96,38 @@ export default class Local {
   }
   public getClima(): Clima {
     return this.clima;
+  }
+  public parseTime(horaEUA: string): string {
+    let time: Array<string> = horaEUA.split(':');
+    let hora: number = Number(time[0]);
+    if (hora - 3 < 0) {
+      for (let i = 3; i >= 1; i--) {
+        hora = hora - 1;
+        if (hora < 0) {
+          hora = 23;
+        }
+      }
+    } else {
+      hora = hora - 3;
+    }
+    const horaFormated: string =
+      hora.toString().length < 2 ? '0'.concat(hora.toString()) : hora.toString();
+    time[0] = horaFormated;
+
+    return time.join(':');
+  }
+  public uvLevel(uv: number): IUvDados {
+    switch (uv) {
+      case 0:
+      case 1:
+      case 2:
+        return this.uvDados[0];
+      case 3:
+      case 4:
+      case 5:
+        return this.uvDados[1];
+      default:
+        throw new Error('Tipo nao existente');
+    }
   }
 }
